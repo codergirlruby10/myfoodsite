2332b111ac8a7c6f97ce571d37dd2ea0e0f194ad
ruby.alesi87@gmail.com <ruby.alesi87@gmail.com>
2024-07-27 13:53:56 -0700

    Created style sheet

--- food/urls.py
+++ food/urls.py
@@ -1,11 +1,14 @@
 from . import views
 from django.urls import path
 
-
-urlpatterns = [    
+app_name = 'food'
+urlpatterns = [  
+    
+    #/food/
     path("", views.index, name = "index"),
-
-path("item", views.item, name = "item"),
-
-path("random", views.random, name ="random"),
+    
+    #/food/1
+    path('<int:item_id>/', views.detail,name='detail'),
+    path("item", views.item, name = "item"),
+  
 ]
