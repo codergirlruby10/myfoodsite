cf30ced6759d385a8924021dfba5df843e23ee36
ruby.alesi87@gmail.com <ruby.alesi87@gmail.com>
2024-07-28 12:35:35 -0700

    Added navbar and more stylization

--- food/models.py
+++ food/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
@@ -10,3 +11,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image= models.CharField(max_length=500,default="https://cdn.dribbble.com/users/1012566/screenshots/4187820/media/985748436085f06bb2bd63686ff491a5.jpg?resize=400x300&vertical=center")
